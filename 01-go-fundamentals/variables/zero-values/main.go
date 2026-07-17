c8c5a04d297a7e5b335f21a07c2e21ee114dc2f6
goke <oyetunjie5@gmail.com>
2026-07-17 18:48:47 +0100

    zero-valued variable, give the default value

---
+++ 01-go-fundamentals/variables/zero-values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+	var isDeveloper bool
+	var salary float64
+
+	fmt.Printf("Name: %q\n", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Developer:", isDeveloper)
+	fmt.Println("Salary:", salary)
+}
