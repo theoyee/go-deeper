bb2b027eeec0d6c619db37820aa047e852f48ab3
goke <oyetunjie5@gmail.com>
2026-07-17 18:57:30 +0100

    learn: constants

---
+++ 01-go-fundamentals/variables/constants/README.md
@@ -0,0 +1,9 @@
+# Constants
+
+## What I Learned
+
+Constants are values that cannot be changed after declaration.
+
+```go
+const maxUsers = 100
+```
---
+++ 01-go-fundamentals/variables/constants/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const appName = "Go-deeper"
+const version = 1
+
+func main() {
+	fmt.Println(appName)
+	fmt.Println(version)
+}
